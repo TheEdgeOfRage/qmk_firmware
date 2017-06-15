5e1bc356ca0f18da8146f3b00fbb8110844f0c2d
Pavle Portic <pavle.portic@gmail.com>
2017-06-15 12:41:14 +0200

    [keymap/raging] Add function button to left half

--- keyboards/ergodox/keymaps/raging/keymap.c
+++ keyboards/ergodox/keymaps/raging/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,			KC_Q,		KC_W,		KC_F,		KC_P,		KC_G,		KC_LPRN,
 		KC_LCTL,		KC_A,		KC_R,		KC_S,		KC_T,		KC_D,
 		KC_LSFT,		KC_Z,		KC_X,		KC_C,		KC_V,		KC_B,		KC_RPRN,
-		XXXXX,			XXXXX,		XXXXX,		XXXXX,		KC_LGUI,
+		TG(FN),			XXXXX,		XXXXX,		XXXXX,		KC_LGUI,
 																		KC_ESC,		TG(QWER),
 																					KC_SLCK,
 															KC_SPC,		KC_LALT,	KC_CAPS,
