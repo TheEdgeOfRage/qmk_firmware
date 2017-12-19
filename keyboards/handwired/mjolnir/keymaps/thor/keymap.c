cc9289cdad9b3cc6bf86b5bb5e26a8b865d544bd
Pavle Portic <pavle.portic@tilda.center>
2017-12-19 13:31:22 +0100

    Rename layers

--- keyboards/handwired/mjolnir/keymaps/thor/keymap.c
+++ keyboards/handwired/mjolnir/keymaps/thor/keymap.c
@@ -1,9 +1,9 @@
 #include "mjolnir.h"
 
-#define _COL 0 // default layer
-#define _QWE 1 // qwerty layout
-#define _FN 2 // function
-#define _RGB 3 // RGB
+#define L_BS 0 // default layer
+#define L_QW 1 // qwerty layout
+#define L_FN 2 // function
+#define L_RGB 3 // RGB
 
 #define _____ KC_TRNS
 #define XXXXX KC_NO
@@ -24,12 +24,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * `--------------------------------------'     `--------------------------------------'
 	 *
 	 */
-	[_COL] = KEYMAP(
+	[L_BS] = KEYMAP(
 		KC_GRV,		KC_1,			KC_2,			KC_3,			KC_4,			KC_5,			KC_6,			KC_7,			KC_8,			KC_9,			KC_0,			KC_MINS,
 		KC_TAB,		KC_Q,			KC_W,			KC_F,			KC_P,			KC_G,			KC_J,			KC_L,			KC_U,			KC_Y,			KC_SCLN,	KC_QUOT,
 		KC_LCTL,	KC_A,			KC_R,			KC_S,			KC_T,			KC_D,			KC_H,			KC_N,			KC_E,			KC_I,			KC_O,			KC_ENT,
 		KC_LSFT,	KC_Z,			KC_X,			KC_C,			KC_V,			KC_B,			KC_K,			KC_M,			KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,
-		XXXXX,		KC_ESC,		KC_LALT,	KC_LGUI,			KC_BSPC,							KC_SPC,				MO(_FN),	KC_PSCR,	MO(_RGB),	_____
+		RGB_SMOD,	KC_ESC,		KC_LALT,	KC_LGUI,			KC_BSPC,							KC_SPC,				MO(L_FN),	KC_PSCR,	MO(L_RGB),	TG(L_RGB)
 	),
 
 	/* Keymap 1: QWERTY layer
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * `--------------------------------------'     `--------------------------------------'
 	 *
 	 */
-	[_QWE] = KEYMAP(
+	[L_QW] = KEYMAP(
 		_____,		_____,		_____,		_____,		_____,		_____,		_____,		_____,		_____,		_____,		_____,		_____,
 		_____,		KC_Q,			KC_W,			KC_E,			KC_R,			KC_T,			KC_Y,			KC_U,			KC_I,			KC_O,			KC_P,			_____,
 		_____,		KC_A,			KC_S,			KC_D,			KC_F,			KC_G,			KC_H,			KC_J,			KC_K,			KC_L,			KC_SCLN,	_____,
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * `--------------------------------------'     `--------------------------------------'
 	 *
 	 */
-	[_FN] = KEYMAP(
+	[L_FN] = KEYMAP(
 		KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,		KC_F11,		KC_F12,
 		_____,		KC_MUTE,	KC_VOLD,	KC_VOLU,	XXXXX,		XXXXX,		XXXXX,		XXXXX,		KC_UP,		XXXXX,		XXXXX,		_____,
 		_____,		KC_MPLY,	KC_MPRV,	KC_MNXT,	XXXXX,		XXXXX,		XXXXX,		KC_LEFT,	KC_DOWN,	KC_RIGHT,	XXXXX,		_____,
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		RESET,		_____,		_____,		_____,				KC_DEL,							_____,					_____,		_____,		_____,		_____
 	),
 
-	[_RGB] = KEYMAP(
+	[L_RGB] = KEYMAP(
 		_____,		RGB_HUI,	RGB_SAI,	RGB_VAI,	_____,		_____,		_____,		_____,		_____,		_____,		_____,		_____,
 		_____,		RGB_HUD,	RGB_SAD,	RGB_VAD,	_____,		_____,		_____,		_____,		_____,		_____,		_____,		_____,
 		_____,		RGB_TOG,	RGB_SMOD,	_____,		_____,		_____,		_____,		_____,		_____,		_____,		_____,		_____,
