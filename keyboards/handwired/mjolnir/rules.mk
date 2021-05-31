641eff5c82a79ce1eabad6e5362f2832fe72df9a
Pavle Portic <git@theedgeofrage.com>
2021-05-31 10:30:38 +0200

    Update mjolnir to latest codebase

--- keyboards/handwired/mjolnir/rules.mk
+++ keyboards/handwired/mjolnir/rules.mk
@@ -66,4 +66,3 @@ UNICODE_ENABLE ?= yes        # Unicode
 BLUETOOTH_ENABLE ?= no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE ?= no           # Audio output on port C6
 RGBLIGHT_ENABLE ?= yes       # WS2812 support
-
