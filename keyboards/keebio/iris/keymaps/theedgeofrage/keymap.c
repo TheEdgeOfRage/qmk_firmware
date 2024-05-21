355e27dcf7d2ad8d2bfbb87d2e4de56e2960a594
TheEdgeOfRage <git@theedgeofrage.com>
2024-05-21 11:16:20 +0200

    Add unicode to theedgeofrage iris map

--- keyboards/keebio/iris/keymaps/theedgeofrage/keymap.c
+++ keyboards/keebio/iris/keymaps/theedgeofrage/keymap.c
@@ -7,15 +7,47 @@ enum my_layers {
 };
 
 #define XXXXXXX KC_NO
-#define QWERTY  MO(_QWERTY)
+#define QWERTY  TG(_QWERTY)
 #define FN      MO(_FN)
 #define CTRL    LCTL_T(KC_ESC)
+
+// #define XZH XP(LZH, UZH)
+// #define XSH XP(LSH, USH)
+// #define XDJ XP(LDJ, UDJ)
+// #define XCH XP(LCH, UCH)
+// #define XCJ XP(LCJ, UCJ)
 
 enum custom_keycodes {
     M_EMAIL = SAFE_RANGE,
     M_USER,
     M_HOST,
     M_ALTC,
+};
+
+enum unicode_names {
+    USH,
+    LSH,
+    UDJ,
+    LDJ,
+    UCJ,
+    LCJ,
+    UCH,
+    LCH,
+    UZH,
+    LZH,
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [USH] = 0x160, // Š
+    [LSH] = 0x161, // š
+    [UDJ] = 0x110, // Đ
+    [LDJ] = 0x111, // đ
+    [UCJ] = 0x106, // Ć
+    [LCJ] = 0x107, // ć
+    [UCH] = 0x10C, // Č
+    [LCH] = 0x10D, // č
+    [UZH] = 0x17D, // Ž
+    [LZH] = 0x17E, // ž
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,11 +84,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                              KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, QK_BOOT,                            KC_EQL,  KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, _______,
+     _______, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, QK_BOOT,                            KC_EQL,  KC_LBRC, KC_RBRC, UP(LZH, UZH),     XXXXXXX,     XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_MPLY, KC_MPRV, KC_MNXT, M_USER,  M_HOST,                             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______,
+     _______, KC_MPLY, KC_MPRV, KC_MNXT, M_USER,  M_HOST,                             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,     XXXXXXX,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, M_EMAIL, RGB_TOG, M_ALTC,  XXXXXXX, XXXXXXX, _______,          _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, _______,
+     _______, M_EMAIL, RGB_TOG, M_ALTC,  QWERTY,  XXXXXXX, _______,          _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, KC_DEL,                    _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -64,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-    // set_unicode_input_mode(UNICODE_MODE_LINUX);
+    set_unicode_input_mode(UNICODE_MODE_LINUX);
 };
 
 // Runs constantly in the background, in a loop.
@@ -108,4 +140,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     return true;
 }
-
